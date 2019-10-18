b7e186a2133f9a825a202822cecb3d03dfcb59d4
0xflotus <0xflotus@gmail.com>
2019-10-18 11:32:33 +0200

    improved jsonbox client

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,19 @@ import axios from 'axios';
 
 interface JsonBoxConfig {
     sort?: string;
-    skip?: string;
-    limit?: string;
+    skip?: number;
+    limit?: number;
     query?: string;
 }
 
 export class JsonBox {
     private static RECORD_ID_KEY: string = "_id";
-    private jsonboxhost: string;
 
-    constructor(jsonboxhost: string = "https://jsonbox.io") {
-        this.jsonboxhost = jsonboxhost;
-    }
+    constructor(private readonly jsonboxhost: string = "https://jsonbox.io") { }
 
-    private getUrl(boxId: string, collection?: string, config?: JsonBoxConfig): string {
+    private getUrl(boxId: string, collection?: string, config: JsonBoxConfig = {
+        sort: '-_createdOn', skip: 0, limit: 20
+    }): string {
         let url = `${this.jsonboxhost}/${boxId}`;
         const { sort, skip, limit, query } = config;
 
@@ -23,7 +22,7 @@ export class JsonBox {
             url = `${url}/${collection}`;
         }
 
-        let parameters = new Map<string, string>();
+        let parameters = new Map<string, string | number>();
         if (sort) {
             parameters.set("sort", sort);
         }
@@ -48,23 +47,23 @@ export class JsonBox {
         return Array.isArray(data) ? data.map(record => record[JsonBox.RECORD_ID_KEY]) : data[JsonBox.RECORD_ID_KEY];
     }
 
-    public async read(boxId: string, collection?: string, config: JsonBoxConfig = {}) {
+    public async read(boxId: string, collection?: string, config?: JsonBoxConfig) {
         const response = await axios.get(this.getUrl(boxId, collection, config));
         return response.status === 200 ? response.data : false;
     }
 
     public async create(data: any, boxId: string, collection?: string) {
-        const response = await axios.post(this.getUrl(boxId, collection, {}), data);
+        const response = await axios.post(this.getUrl(boxId, collection), data);
         return response.status === 200 ? response.data : false;
     }
 
     public async update(data: any, boxId: string, recordId: string) {
-        const response = await axios.put(this.getUrl(boxId, recordId, {}), data);
+        const response = await axios.put(this.getUrl(boxId, recordId), data);
         return response.status === 200 ? response.data : false;
     }
 
     public async delete(boxId: string, recordId: string) {
-        const response = await axios.delete(this.getUrl(boxId, recordId, {}));
+        const response = await axios.delete(this.getUrl(boxId, recordId));
         return response.status === 200 ? response.data : false;
     }
 
