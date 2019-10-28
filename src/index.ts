8c7c7502c82016006c297656398852c0014b1cdd
0xflotus <0xflotus@gmail.com>
2019-10-29 00:02:30 +0100

    v1.0.7

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ interface JsonBoxConfig {
 export class JsonBox {
     private static RECORD_ID_KEY: string = "_id";
 
-    constructor(private readonly jsonboxhost: string = "https://jsonbox.io") { }
+    constructor(private readonly jsonboxhost: string = "https://jsonbox.io",
+        private readonly axiosConfig?: AxiosRequestConfig) { }
 
     private getUrl(boxId: string, collection?: string, config: JsonBoxConfig = {
         sort: '-_createdOn', skip: 0, limit: 20
@@ -47,27 +48,27 @@ export class JsonBox {
         return Array.isArray(data) ? data.map(record => record[JsonBox.RECORD_ID_KEY]) : data[JsonBox.RECORD_ID_KEY];
     }
 
-    public async read(boxId: string, collection?: string, config?: JsonBoxConfig, axiosConfig?: AxiosRequestConfig) {
-        const response = await axios.get(this.getUrl(boxId, collection, config), axiosConfig);
+    public async read(boxId: string, collection?: string, config?: JsonBoxConfig) {
+        const response = await axios.get(this.getUrl(boxId, collection, config), this.axiosConfig);
         return response.status === 200 ? response.data : false;
     }
 
-    public async create(data: any, boxId: string, collection?: string, axiosConfig?: AxiosRequestConfig) {
-        const response = await axios.post(this.getUrl(boxId, collection), data, axiosConfig);
+    public async create(data: any, boxId: string, collection?: string) {
+        const response = await axios.post(this.getUrl(boxId, collection), data, this.axiosConfig);
         return response.status === 200 ? response.data : false;
     }
 
-    public async update(data: any, boxId: string, recordId: string, axiosConfig?: AxiosRequestConfig) {
-        const response = await axios.put(this.getUrl(boxId, recordId), data, axiosConfig);
+    public async update(data: any, boxId: string, recordId: string) {
+        const response = await axios.put(this.getUrl(boxId, recordId), data, this.axiosConfig);
         return response.status === 200 ? response.data : false;
     }
 
-    public async delete(boxId: string, recordId: string, axiosConfig?: AxiosRequestConfig) {
-        const response = await axios.delete(this.getUrl(boxId, recordId), axiosConfig);
+    public async delete(boxId: string, recordId: string) {
+        const response = await axios.delete(this.getUrl(boxId, recordId), this.axiosConfig);
         return response.status === 200 ? response.data : false;
     }
 
-    public async deleteMany(boxId: string, recordIds: string[], axiosConfig?: AxiosRequestConfig) {
-        return recordIds.map(id => this.delete(boxId, id, axiosConfig));
+    public async deleteMany(boxId: string, recordIds: string[]) {
+        return recordIds.map(id => this.delete(boxId, id));
     }
 } 
